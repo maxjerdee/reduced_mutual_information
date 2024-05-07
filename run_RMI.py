250a68991ba411bcc497a066ea09e190e37d6c0a
maxjerdee <maximilian2468@gmail.com>
2024-05-07 19:36:58 -0400

    Formatting print statements

--- run_RMI.py
+++ run_RMI.py
@@ -21,5 +21,23 @@ label_pairs = np.loadtxt(filename, dtype='str')
 true_labels = label_pairs[:,0]
 candidate_labels = label_pairs[:,1]
 
-print(compute_RMI(true_labels, candidate_labels, reduction='DM', normalization='asymmetric', verbose=True))
-print(compute_RMI(true_labels, candidate_labels, reduction='flat', normalization='asymmetric', verbose=True))
+# Verbose run
+compute_RMI(true_labels, candidate_labels, reduction='DM', normalization='asymmetric', verbose=True)
+print()
+
+# Compute RMIs
+RMI_DM = compute_RMI(true_labels, candidate_labels)
+RMI_flat = compute_RMI(true_labels, candidate_labels, reduction='flat')
+RMI_unreduced = compute_RMI(true_labels, candidate_labels, reduction='none')
+
+print(f"Filename: {filename}")
+print(f"Asymmetrically normalized reduced mutual informations:")
+print(f"RMI_DM: {RMI_DM:.3f}, RMI_flat: {RMI_flat:.3f}, RMI_unreduced: {RMI_unreduced:.3f}")
+
+# Symmetric normalizations
+RMI_DM_sym = compute_RMI(true_labels, candidate_labels, normalization='symmetric')
+RMI_flat_sym = compute_RMI(true_labels, candidate_labels, reduction='flat', normalization='symmetric')
+RMI_unreduced_sym = compute_RMI(true_labels, candidate_labels, reduction='none', normalization='symmetric')
+
+print(f"Symmetrically normalized reduced mutual informations:")
+print(f"RMI_DM: {RMI_DM_sym:.3f}, RMI_flat: {RMI_flat_sym:.3f}, RMI_unreduced: {RMI_unreduced_sym:.3f}")
